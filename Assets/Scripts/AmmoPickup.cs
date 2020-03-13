6deaf98376407177dbbb8eb5cb1fcfe9e7870300
Michael Numrich <mnumrich@gmail.com>
2020-03-12 18:11:06 -0600

    Started ammo pickup

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    [SerializeField] int ammoAmount = 5;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
