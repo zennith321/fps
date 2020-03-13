59f48809bbe5ae319c3084f739eb6a6ef2e92165
Michael Numrich <pc@pop-os.localdomain>
2020-03-12 22:52:21 -0600

    Finished all three ammo pickups

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class AmmoPickup : MonoBehaviour
 {
     [SerializeField] int ammoAmount = 5;
+
+    [SerializeField] AmmoType ammoType;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            FindObjectOfType<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
             Destroy(gameObject);
         }
     }
